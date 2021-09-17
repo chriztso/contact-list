4e0cb57c9b2ad48fe9d19c7ac6ffd2988ddeeab9
Christopher  Tso <christophertso@Christophers-iMac.local>
2021-09-17 14:16:17 -0500

    Edited formatting

--- src/App.js
+++ src/App.js
@@ -14,49 +14,49 @@ const App = () => {
   useEffect(() => {
     const getData = async () => {
       try {
-      const requestOptions = {
-        headers: {
-          'Api-Token': 'bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0'
+        const requestOptions = {
+          headers: {
+            'Api-Token': 'bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0'
+          }
         }
-      }
-      const res = await fetch(
-        'https://cors-anywhere.herokuapp.com/https://sahmed93846.api-us1.com/api/3/contacts?include=geoIps.geoAddress,contactTags.tag,deals', 
-        requestOptions
-      )
-      const data = await res.json()
-      const { tags, contactTags, deals, geoAddresses, geoIps, contacts } = data
+        const res = await fetch(
+          'https://cors-anywhere.herokuapp.com/https://sahmed93846.api-us1.com/api/3/contacts?include=geoIps.geoAddress,contactTags.tag,deals',
+          requestOptions
+        )
+        const data = await res.json()
+        const { tags, contactTags, deals, geoAddresses, geoIps, contacts } = data
 
-      //create object with tag IDs as keys and tag objects as values
-      const tagsObj = formatObjectByIDKeys(tags)
-      setTagsObject(tagsObj)
+        //create object with tag IDs as keys and tag objects as values
+        const tagsObj = formatObjectByIDKeys(tags)
+        setTagsObject(tagsObj)
 
-      //create object with contact tag IDs as keys and contact tag objects as values
-      const contactTagsObj = formatObjectByIDKeys(contactTags)
-      setContactTagsObject(contactTagsObj)
+        //create object with contact tag IDs as keys and contact tag objects as values
+        const contactTagsObj = formatObjectByIDKeys(contactTags)
+        setContactTagsObject(contactTagsObj)
 
-      //create object with deal IDs as keys and deal objects as values
-      const dealsObj = formatObjectByIDKeys(deals)
-      setDealsObject(dealsObj)
-      
-       //create object with geoAddress IDs as keys and geoAddress objects as values
-       const geoAddressesObj = formatObjectByIDKeys(geoAddresses)
-       setGeoAddressesObject(geoAddressesObj)
+        //create object with deal IDs as keys and deal objects as values
+        const dealsObj = formatObjectByIDKeys(deals)
+        setDealsObject(dealsObj)
 
-      //create object with geoIps IDs as keys and geoIps objects as values
-      const geoIpsObj = formatObjectByIDKeys(geoIps)
-      setGeoIpsObject(geoIpsObj)
-    
-      setContacts(contacts)
+        //create object with geoAddress IDs as keys and geoAddress objects as values
+        const geoAddressesObj = formatObjectByIDKeys(geoAddresses)
+        setGeoAddressesObject(geoAddressesObj)
+
+        //create object with geoIps IDs as keys and geoIps objects as values
+        const geoIpsObj = formatObjectByIDKeys(geoIps)
+        setGeoIpsObject(geoIpsObj)
+
+        setContacts(contacts)
       } catch (e) {
         console.error(e)
       }
-    } 
+    }
     getData()
   }, [])
 
   return (
     <div className="App">
-      <ContactsList 
+      <ContactsList
         contacts={contacts}
         dealsObject={dealsObject}
         tagsObject={tagsObject}
