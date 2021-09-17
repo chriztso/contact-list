eb259460ee6d3d9b84c06036711a51ece1c93b2f
Christopher  Tso <christophertso@Christophers-iMac.local>
2021-09-17 14:00:11 -0500

    Removed console logs

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ const App = () => {
 
   useEffect(() => {
     const getData = async () => {
+      try {
       const requestOptions = {
         headers: {
           'Api-Token': 'bcd062dedabcd0f1ac8a568cdcf58660c44d7e79b91763cc1a5d0c03d52c522d851fceb0'
@@ -23,35 +24,32 @@ const App = () => {
         requestOptions
       )
       const data = await res.json()
-      console.log("DATA", data)  
       const { tags, contactTags, deals, geoAddresses, geoIps, contacts } = data
 
       //create object with tag IDs as keys and tag objects as values
       const tagsObj = formatObjectByIDKeys(tags)
-      console.log("TAGS OBJ", tagsObj)
       setTagsObject(tagsObj)
 
       //create object with contact tag IDs as keys and contact tag objects as values
       const contactTagsObj = formatObjectByIDKeys(contactTags)
-      console.log("CONTACTS TAG OBJ", contactTagsObj)
       setContactTagsObject(contactTagsObj)
 
       //create object with deal IDs as keys and deal objects as values
       const dealsObj = formatObjectByIDKeys(deals)
-      console.log("DEALS obj", dealsObj)
       setDealsObject(dealsObj)
       
        //create object with geoAddress IDs as keys and geoAddress objects as values
        const geoAddressesObj = formatObjectByIDKeys(geoAddresses)
-       console.log("GeoAddresses obj", geoAddressesObj)
        setGeoAddressesObject(geoAddressesObj)
 
       //create object with geoIps IDs as keys and geoIps objects as values
       const geoIpsObj = formatObjectByIDKeys(geoIps)
-      console.log("geoIps OBJ", geoIpsObj)
       setGeoIpsObject(geoIpsObj)
     
       setContacts(contacts)
+      } catch (e) {
+        console.error(e)
+      }
     } 
     getData()
   }, [])
